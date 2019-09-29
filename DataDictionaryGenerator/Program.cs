091ae9942601ed5ae150879bbd23781b49bb4755
zp <zengpeng@ali-sys.com>
2019-09-29 19:04:50 +0800

    23

--- DataDictionaryGenerator/Program.cs
+++ DataDictionaryGenerator/Program.cs
@@ -15,7 +15,7 @@ namespace DataDictionaryGenerator
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(false);  
             Application.Run(new frmMain());
         }
     }
