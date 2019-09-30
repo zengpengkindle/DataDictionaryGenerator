b82317ea1b9ed05bb63d10045510f6af99eb7340
zp <zengpeng@ali-sys.com>
2019-09-30 09:59:00 +0800

    修改配置文件

--- DataDictionaryGenerator/Program.cs
+++ DataDictionaryGenerator/Program.cs
@@ -9,7 +9,7 @@ namespace DataDictionaryGenerator
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。 fuck二 
+        /// 应用程序的主入口点。 
         /// </summary>
         [STAThread]
         static void Main()  
